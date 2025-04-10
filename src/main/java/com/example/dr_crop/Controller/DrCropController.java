bdeef67eda6cdb6460867cf37549752f52ad3707
Ayush <ayushmanjena24@gmail.com>
2025-04-10 10:09:53 +0530

    updated templates

--- src/main/java/com/example/dr_crop/Controller/DrCropController.java
+++ src/main/java/com/example/dr_crop/Controller/DrCropController.java
@@ -23,68 +23,16 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 
 @Controller
-public class DrCropController { // Might delete this
-
-    @Autowired
-    private ConditionService conditionService;
+public class DrCropController {
 
     @GetMapping("/")
     public String home(){
         return "index";
     }
 
-    @GetMapping("/condition")
-    public String conditionForm(){
-        return "upload-image";
+    @GetMapping("/store")
+    public String storePage(){
+        return "redirect:/web/store/all";
     }
 
-//    @PostMapping("/uploadweb")
-//    public String uploadImage(@RequestParam("file") MultipartFile file, RedirectAttributes redirectAttributes){
-//
-//        String fileName = "abcde";// conditionService.uploadImage(file, redirectAttributes, userId);
-//
-//        if (fileName != null){
-//            // if the image is valid send it to the python app
-//
-//            String base64img = "abcde";
-//
-//            ConditionResult conditionResult = conditionService.sendImageToMLLayer(fileName);
-//
-//            if(conditionResult != null){
-//                System.out.println("Plant name : " +conditionResult.plantName);
-//                System.out.println("Condition name : " +conditionResult.conditionName);
-//
-//                // save the condition results to a pdf of the same name
-//            }
-//            return "redirect:download"; // might need changes if output is valid or invalid
-//        }
-//        return "redirect:download";
-//    }
-
-    @GetMapping("/download") // display the page where you can download the file
-    public String downloadPage(){
-        return "condition-results";
-    }
-
-    @GetMapping("/downloadfileweb") // to download the pdf report
-    public ResponseEntity<InputStreamResource> downloadPdfFile() throws IOException {
-
-        String id = "abcde.pdf";
-
-        String directory = "D:/DR-CROP/dr-crop/output-storage/";
-        File file = new File(directory + id);
-
-        if (!file.exists()) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
-        }
-
-        MediaType mediaType = MediaType.parseMediaType("application/pdf");
-        InputStreamResource resource = new InputStreamResource(new FileInputStream(file));
-
-        return ResponseEntity.ok()
-                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=" + file.getName())
-                .contentType(mediaType)
-                .contentLength(file.length())
-                .body(resource);
-    }
 }
